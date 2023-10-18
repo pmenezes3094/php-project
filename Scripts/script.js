63d8975a9dbc61f521782800e76eddd5aa4a22d2
pmenezes3094 <pmene.nic@gmail.com>
2023-10-17 18:09:57 -0700

    Define the functionality of what happens when the save button is clicked

--- Scripts/script.js
+++ Scripts/script.js
@@ -22,3 +22,15 @@ function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
 
+// Function to save the URL (you can customize this part)
+function saveURL() {
+    // Retrieve values from form fields
+    const url = document.getElementById("url").value;
+    const linkName = document.getElementById("linkName").value;
+    const tags = document.getElementById("tags").value;
+
+    // logic to handle the data comes here
+
+    // Close the modal
+    closeModal('urlModal');
+}
