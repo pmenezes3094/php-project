7251ec3a696417fcb86c230c5719fc867b25e712
pmenezes3094 <pmene.nic@gmail.com>
2023-10-17 18:08:15 -0700

    Define functionality to close modal

--- Scripts/script.js
+++ Scripts/script.js
@@ -16,3 +16,9 @@ function submitForm() {
 function openModal(modalId) {
     document.getElementById(modalId).style.display = "block";
 }
+
+// Function to close the modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
