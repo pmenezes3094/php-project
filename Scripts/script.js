65ca62120bb1d915e9a24ae8787cc681a8df87ec
pmenezes3094 <pmene.nic@gmail.com>
2023-10-17 18:07:38 -0700

    Define the functionality to open modal

--- Scripts/script.js
+++ Scripts/script.js
@@ -9,3 +9,10 @@ function submitForm() {
     //Redirect to workspace page
     window.location.href = "workspace.html";
 }
+
+/*Modal Related Code */
+
+// Function to open the modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
