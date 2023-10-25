1abdbdd422222339a1d78605e3e5fdfb1f4ed471
Priya Menezes <113142516+pmenezes3094@users.noreply.github.com>
2023-10-25 11:44:10 -0700

    Add the close function to the file

--- Scripts/onLoad.js
+++ Scripts/onLoad.js
@@ -2,4 +2,8 @@ function myFunction() {
     document.getElementById('loginSuccessfulModal').style.display = "block";
 }
 
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
 window.onload = myFunction;
