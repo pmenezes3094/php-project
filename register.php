5d998e0bb25933bf2b0a371ea2e3a9958e7bbde4
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 16:06:09 -0700

    Remove non-necessary comments

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
-$username = 'da'; // Your MySQL username
-$password = 123; // Your MySQL password
+$username = 'da'; 
+$password = 123; 
 
 $conn = new PDO($dsn, $username, $password);
 
--- reset.php
+++ reset.php
@@ -1,5 +1,6 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
-$username = 'da'; // Your MySQL username
-$password = 123; // Your MySQL password
+$username = 'da'; 
+$password = 123; 
+
 ?>
