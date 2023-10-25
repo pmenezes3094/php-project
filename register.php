4d5e87f24fb6d0a7c06e07a90e7d03975c96654f
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 15:11:42 -0700

    assign database connection parameters

--- register.php
+++ register.php
@@ -1,8 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "phpproject";
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$username = 'da'; // Your MySQL username
+$password = 123; // Your MySQL password
 
 $conn = new PDO($dsn, $username, $password);
 
