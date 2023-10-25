5626e93c19e4332f3372690831e5c46a37af577d
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 15:14:42 -0700

    remove database closing connection

--- register.php
+++ register.php
@@ -11,6 +11,4 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 $sql = "INSERT INTO user (fullname, email, username, password) VALUES ('$fullname', '$email', '$username', '$password')";
-
-$conn->close();
 ?>
