2b9fc23216c6cb3f0846b1b1389e9fb053983518
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 16:58:40 -0700

    Add alert statements for successful or unscuccessful form submission

--- logincheck.php
+++ logincheck.php
@@ -6,4 +6,13 @@ $password = 123;
 $conn = new PDO($dsn, $username, $password);
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($stmt->execute()) {
+    echo '<script>alert("Login Successful");</script>';
+    echo '<script>window.location.href = "workspace.php";</script>';
+} 
+else 
+{
+    echo '<script>alert("Error: ' . $stmt->errorInfo()[2] . '");</script>';
+}
 ?>
