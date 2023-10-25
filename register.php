afbd4dab36815514d39c170925df4a10ae752155
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 15:17:40 -0700

    bind parameters

--- register.php
+++ register.php
@@ -15,4 +15,10 @@ $password = $_POST['password'];
 $sql = "INSERT INTO user (fullname, email, username, password) VALUES (:fullname, :email, :username, :password)";
 
 $stmt = $conn->prepare($sql);
+
+$stmt->bindParam(':fullname', $fullname, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+$stmt->bindParam(':password', $password, PDO::PARAM_STR);
+
 ?>
