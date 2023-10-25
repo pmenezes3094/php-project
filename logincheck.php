b65662b385a605a9a6ae64584743d7d6791a5139
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 16:56:58 -0700

    Add statments to make database connections

---
+++ logincheck.php
@@ -0,0 +1,9 @@
+<?php
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$username = 'da'; 
+$password = 123; 
+
+$conn = new PDO($dsn, $username, $password);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
