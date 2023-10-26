d1423c076b250d1a826d38147e208fe21aa072e4
pmenezes3094 <pmene.nic@gmail.com>
2023-10-26 12:59:21 -0700

    add algorithm to build card creation logic

---
+++ Php/controller/workspace.php
@@ -0,0 +1,11 @@
+<?php
+//start session and save username for dataretrival
+//make connection with database
+//create and execute a select query to retrive all text notes
+//check for number of results
+    //if more than one result store in array variable
+        // run a loop until end of array
+            // store card html structure in variable along with retrived text note
+            //on workspace page create card using that variavle
+    //else do nothing
+?>
