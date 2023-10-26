c04282ea8a69df95b88c24e18c295094a1de658d
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 17:25:13 -0700

    Comment alerts to focus on data retrival and match logic

--- logincheck.php
+++ logincheck.php
@@ -7,12 +7,12 @@ $conn = new PDO($dsn, $username, $password);
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if ($stmt->execute()) {
-    echo '<script>alert("Login Successful");</script>';
-    echo '<script>window.location.href = "workspace.php";</script>';
-} 
-else 
-{
-    echo '<script>alert("Error: ' . $stmt->errorInfo()[2] . '");</script>';
-}
+// if ($stmt->execute()) {
+//     echo '<script>alert("Login Successful");</script>';
+//     echo '<script>window.location.href = "workspace.php";</script>';
+// } 
+// else 
+// {
+//     echo '<script>alert("Error: ' . $stmt->errorInfo()[2] . '");</script>';
+// }
 ?>
