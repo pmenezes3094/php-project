8a50c91f20c978ea0ece66e50ed65347a2851922
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 18:09:50 -0700

    Add my algorithm to work out retrival and match logic for password check

--- logincheck.php
+++ logincheck.php
@@ -20,12 +20,20 @@ $user = $stmt->fetch();
 if ($user) 
 {
     $password = $user['password'];
+
+    //Algorithm to check password
+        //store password from input field in a variable
+        //store password from database in another vaiabe
+        //check login if both password match 
+        //if match -> alert login successful
+            //echo '<script>alert("Login Successful");</script>';
+            //echo '<script>window.location.href = "workspace.php";</script>';
+        // else echo '<script>alert("Error: ' . $stmt->errorInfo()[2] . '");</script>';
 } 
 else 
 {
     echo '<script>alert("User does not exist");</script>';
 }
-
 // if ($stmt->execute()) {
 //     echo '<script>alert("Login Successful");</script>';
 //     echo '<script>window.location.href = "workspace.php";</script>';
