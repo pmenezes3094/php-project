1eb42473af9cf8448a5fca02c9c263d2b94a75f7
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 20:48:07 -0700

    Copy code from genral script page to utitlies specific page

---
+++ Scripts/utilities.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function goToHomePage() {
+    //Redirect to homepage
+    window.location.href = "../../index.php";
+}
+
+function goBack() {
+    //Redirect back to previous page
+    window.history.back();
+}
+
+//Function to validate that password and re-enter password have same fields
+function validatePassword() {
+    var password = document.getElementById('password').value;
+    var repassword = document.getElementById('repassword').value;
+
+    if (password !== repassword) {
+        alert("Passwords do not match. Please re-enter your password.");
+        return false;
+    }
+
+    return true;
+}
