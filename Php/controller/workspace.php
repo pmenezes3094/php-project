21b7f1f2000301130f72bd3ac961c37f44e03d7c
pmenezes3094 <pmene.nic@gmail.com>
2023-10-26 13:01:42 -0700

    make database connection

--- Php/controller/workspace.php
+++ Php/controller/workspace.php
@@ -2,7 +2,9 @@
 session_start(); 
 $username = $_SESSION['username'];
 
-//make connection with database
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$dbusername = 'da'; 
+$dbpassword = 123; 
 //create and execute a select query to retrive all text notes
 //check for number of results
     //if more than one result store in array variable
