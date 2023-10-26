190c2d100ef195e0ae2e21725e0261995159fa3c
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 23:45:05 -0700

    remove test code

--- Php/controller/login.php
+++ Php/controller/login.php
@@ -31,9 +31,6 @@ if ($user)
         $_SESSION['username'] = $user['username'];
         $_SESSION['fullname'] = $user['fullname'];
 
-        $test = $_SESSION['fullname'];
-
-        echo '<script>alert("' . $test . '");</script>';
         echo '<script>alert("Login Successful");</script>';
         echo '<script>window.location.href = "../view/workspace.view.php";</script>';
     }
