d350c1fea2881a4ccc23e4a2f179e7841f155b84
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 20:38:09 -0700

    create dummy cards partials file and include it in the workspace page

---
+++ Php/view/partials/dummycards.php
@@ -0,0 +1,38 @@
+<!-- Div 2: Message - "Data Cards" -->
+<div class="card-grid-container">
+    <div class="card-grid-item">
+        <div class="card-content">
+            <img src="../../Assets/demo-image.jpeg" alt="pink flowers">
+        </div>
+    </div>
+            
+    <div class="card-grid-item">
+        <div class="card-content">
+            <p>
+                “And so the spring buds burst, and so I gaze, And so the blossoms fall, and so my days ...”― Onitsura
+            </p>
+        </div>
+    </div>
+
+    <div class="card-grid-item">
+        <div class="card-content">
+            <audio controls>
+                <source src="../../Assets/dry-flowers-and-twigs-rustling-34133.mp3" type="audio/mp3">
+            </audio>
+        </div>
+    </div>
+
+    <div class="card-grid-item">
+        <video controls width="300">
+            <source src="../../Assets/pexels_videos_3744 (Original).mp4" type="video/mp4">
+        </video>
+    </div>
+
+    <div class="card-grid-item">
+        <div class="card-content">
+            <a href="https://www.weloveflorists.com/floral-blogs/" target="_blank">
+                <img src="../../Assets/website-header.png" alt="weloveflorists.com header page which has flowers"> Visit weloveflorists.com
+            </a>
+        </div>
+    </div>
+</div>
