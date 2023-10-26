91c8dec1d8d5f5bbae8647f1aea38ca6d8868d10
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 20:30:23 -0700

    Update card content links

--- Php/view/workspace.view.php
+++ Php/view/workspace.view.php
@@ -22,7 +22,7 @@
         <div class="card-grid-container">
             <div class="card-grid-item">
                 <div class="card-content">
-                    <img src="Assets/demo-image.jpeg" alt="pink flowers">
+                    <img src="../../Assets/demo-image.jpeg" alt="pink flowers">
                 </div>
             </div>
             
@@ -38,21 +38,21 @@
             <div class="card-grid-item">
                 <div class="card-content">
                     <audio controls>
-                        <source src="Assets/dry-flowers-and-twigs-rustling-34133.mp3" type="audio/mp3">
+                        <source src="../../Assets/dry-flowers-and-twigs-rustling-34133.mp3" type="audio/mp3">
                     </audio>
                 </div>
             </div>
 
             <div class="card-grid-item">
                 <video controls width="300">
-                    <source src="Assets/pexels_videos_3744 (Original).mp4" type="video/mp4">
+                    <source src="../../Assets/pexels_videos_3744 (Original).mp4" type="video/mp4">
                 </video>
             </div>
 
             <div class="card-grid-item">
                 <div class="card-content">
                     <a href="https://www.weloveflorists.com/floral-blogs/" target="_blank">
-                        <img src="Assets/website-header.png" alt="weloveflorists.com header page which has flowers"> Visit weloveflorists.com
+                        <img src="../../Assets/website-header.png" alt="weloveflorists.com header page which has flowers"> Visit weloveflorists.com
                     </a>
                 </div>
             </div>
