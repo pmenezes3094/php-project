1aa7276d665b1fff12e4a03b7a293bf363408740
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 23:55:24 -0700

    asaign values to varibales again

--- Php/controller/login.php
+++ Php/controller/login.php
@@ -31,6 +31,9 @@ if ($user)
         $_SESSION['username'] = $user['username'];
         $_SESSION['fullname'] = $user['fullname'];
 
+        $username = $_SESSION['username'];
+        $fullname = $_SESSION['fullname'];
+
         echo '<script>alert("Login Successful");</script>';
         echo '<script>window.location.href = "../view/workspace.view.php";</script>';
     }
