3625bc3ba57ea1ca753fb1f4c99bd242ca6d5837
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 17:05:47 -0700

    remove conceptually erroneous logic file

--- Scripts/onLoad.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    document.getElementById('loginSuccessfulModal').style.display = "block";
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
-}
-
-window.onload = myFunction;
