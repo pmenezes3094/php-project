13d38d5468c21454f0ecfed7edab5ff30f3890de
pmenezes3094 <pmene.nic@gmail.com>
2023-10-25 19:49:38 -0700

    create partial head file and call on all requiredpages

--- Php/view/register.view.php
+++ Php/view/register.view.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../Styles/style.css">
-    <title>Registration Form</title>
-    <script src="../../Scripts/script.js"></script>
-</head>
+<?php require 'partials/htmlhead.php'; ?>
 <body>
     <div class="form-container">
         <div class="container">
