570c25884a1494311134311fbdecf7a2a40118da
pmenezes3094 <pmene.nic@gmail.com>
2023-10-27 15:07:21 -0700

    make database connection

--- Php/controller/deletecard.php
+++ Php/controller/deletecard.php
@@ -1,6 +1,15 @@
 <?php
 $_SESSION['username'] = $user['username'];
-// Create database connection
+
+$stmt = null;
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$dbusername = 'da'; 
+$dbpassword = 123; 
+
+$conn = new PDO($dsn, $dbusername, $dbpassword);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 // grab the note content
 // create sql query to delete record which belongs to particular username(grabed from session) and id(of the selected content)
 //prepare sql query
