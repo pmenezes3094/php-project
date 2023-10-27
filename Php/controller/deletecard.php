111372ca1a060061156e2aa22fa4b72a36d119c9
pmenezes3094 <pmene.nic@gmail.com>
2023-10-27 15:01:45 -0700

    save algorithm to delete card

---
+++ Php/controller/deletecard.php
@@ -0,0 +1,14 @@
+<?php
+//Algorith to delete card
+//Grab username from current session
+// Create database connection
+// grab the note content
+// create sql query to delete record which belongs to particular username(grabed from session) and id(of the selected content)
+//prepare sql query
+//bind parameters
+//execute query 
+// if query executed successfully 
+    // show alert that item deleted sucessfully
+    // redirect page to index workspace page
+//else show alert that card couldnt be deleted. please try again
+?>
