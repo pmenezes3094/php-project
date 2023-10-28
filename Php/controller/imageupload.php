020c2528dd4de591d887d40d04be20331865f9a8
pmenezes3094 <pmene.nic@gmail.com>
2023-10-27 22:52:05 -0700

    saving the directory where the file needs to get uploded

--- Php/controller/imageupload.php
+++ Php/controller/imageupload.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if ($_FILES['image']['error'] === 0)
     {
-        
+        $upload_directory = "../../../php-project-file/image/";   
     }
 }
 ?>
