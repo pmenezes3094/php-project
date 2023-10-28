fcc283019bece49fee3d00af47027efaecc4b3d1
pmenezes3094 <pmene.nic@gmail.com>
2023-10-27 23:40:00 -0700

    create a dbms connection

--- Php/controller/imagedatabaseentry.php
+++ Php/controller/imagedatabaseentry.php
@@ -3,4 +3,12 @@ session_start();
 $username = $_SESSION['username'];
 $filepath = $_SESSION['file_path'];
 
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$dbusername = 'da'; 
+$dbpassword = 123; 
+
+$conn = new PDO($dsn, $dbusername, $dbpassword);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
