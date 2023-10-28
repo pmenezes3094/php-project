1cca69641f32041fe2cee96b5fc26bcf221c9b0a
pmenezes3094 <pmene.nic@gmail.com>
2023-10-27 22:54:38 -0700

    save the list of allowed extensions

--- Php/controller/imageupload.php
+++ Php/controller/imageupload.php
@@ -4,7 +4,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if ($_FILES['image']['error'] === 0)
     {
-        $upload_directory = "../../../php-project-file/image/";   
+        $upload_directory = "../../../php-project-file/image/"; 
+        $allowed_extensions = array("jpg", "jpeg", "png", "gif", "bmp", "tiff", "svg");  
     }
 }
 ?>
