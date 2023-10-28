cfe37f201c84ee13e17e19a5e9bf720766d3b8ca
pmenezes3094 <pmene.nic@gmail.com>
2023-10-27 23:02:53 -0700

    correct naming conventions

--- Php/controller/imageupload.php
+++ Php/controller/imageupload.php
@@ -4,8 +4,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if ($_FILES['image']['error'] === 0)
     {
-        $upload_directory = "../../../php-project-file/image/"; 
-        $allowed_extensions = array("jpg", "jpeg", "png", "gif", "bmp", "tiff", "svg");  
+        $uploaddirectory = "../../../php-project-file/image/"; 
+        $allowedextensions = array("jpg", "jpeg", "png", "gif", "bmp", "tiff", "svg");  
 
         $filename = $_FILES['image']['name'];
         $fileextension = pathinfo($filenname, PATHINFO_EXTENSION);
