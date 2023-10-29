b3dff994587fcb922a04389275c20fb761f7bd04
pmenezes3094 <pmene.nic@gmail.com>
2023-10-28 19:38:37 -0700

    update code from text file to deletecardfile

--- Php/controller/deletecard.php
+++ Php/controller/deletecard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$username = $_SESSION['username'];
+$userId = $_SESSION['userId'];
 
 $stmt = null;
 $dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
@@ -11,11 +11,11 @@ $conn = new PDO($dsn, $dbusername, $dbpassword);
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$id = $_POST['id'];
-$sql = "DELETE FROM usernotes WHERE username = :username AND id = :id";
+$itemId = $_POST['itemId'];
+$sql = "DELETE FROM item WHERE userId = :userId AND itemId = :itemId";
 $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':userId', $userId, PDO::PARAM_STR);
+    $stmt->bindParam(':itemId', $itemId, PDO::PARAM_INT);
 if ($stmt->execute()) 
 {
     echo '<script>alert("Card deleted sucessfully");</script>';
