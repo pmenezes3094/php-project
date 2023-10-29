af5c9e5713fe1607818d159ba6f960349785a9ef
pmenezes3094 <pmene.nic@gmail.com>
2023-10-28 19:46:14 -0700

    grab userId instead if username

--- Php/controller/imagedatabaseentry.php
+++ Php/controller/imagedatabaseentry.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); 
-$username = $_SESSION['username'];
+$userId = $_SESSION['userId'];
 $filepath = $_SESSION['filepath'];
 
 $dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
