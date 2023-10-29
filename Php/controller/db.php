bf7e2ba4d0947aeccd0cd62b15da0f01dd0cd58f
pmenezes3094 <pmene.nic@gmail.com>
2023-10-28 19:45:56 -0700

    remove unwanted db.php partial file

--- Php/controller/db.php
+++
@@ -1,7 +0,0 @@
-$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
-$dbusername = 'da'; 
-$dbpassword = 123; 
-
-$pdo = new PDO($dsn, $dbusername, $dbpassword);
-
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
