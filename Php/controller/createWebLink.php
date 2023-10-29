8bf6fad6992f8c4f0a8790bf2c3af15d9e0e5fd7
pmenezes3094 <pmene.nic@gmail.com>
2023-10-28 21:21:54 -0700

    create and execute insert query

--- Php/controller/createWebLink.php
+++ Php/controller/createWebLink.php
@@ -12,10 +12,16 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $itemDetail = $_POST['webLink'];
 
-$itemTypeSql = "SELECT itemTypeId FROM itemtype WHERE itemtype = 'textNote'";
+$itemTypeSql = "SELECT itemTypeId FROM itemtype WHERE itemtype = 'webLink'";
 $itemTypeStmt = $conn->prepare($itemTypeSql);
 $itemTypeStmt->execute();
 $itemTypeResult = $itemTypeStmt->fetch();
 $itemTypeId = $itemTypeResult['itemTypeId'];
+
+$sql = "INSERT INTO item (itemDetail, itemTypeId, userId) VALUES (:itemDetail, :itemTypeId, :userId)";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':itemDetail', $itemDetail, PDO::PARAM_STR);
+$stmt->bindParam(':itemTypeId', $itemTypeId, PDO::PARAM_INT);
+$stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
 
 ?>
