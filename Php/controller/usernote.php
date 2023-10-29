f3f8c81089f55aa54127028406db89479cd629c1
pmenezes3094 <pmene.nic@gmail.com>
2023-10-28 17:24:13 -0700

    correct linked details

--- Php/controller/usernote.php
+++ Php/controller/usernote.php
@@ -1,43 +1,39 @@
 <?php
-session_start(); 
+session_start();
 $username = $_SESSION['username'];
 
 $dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
-$dbusername = 'da'; 
-$dbpassword = 123; 
+$dbusername = 'da';
+$dbpassword = 123;
 
 $conn = new PDO($dsn, $dbusername, $dbpassword);
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$userIdSql = "SELECT userId FROM user WHERE username = $username";
-$stmt = $conn->prepare($userIdSql);
-$stmt->bindParam(':username', $username, PDO::PARAM_STR);
-$stmt->execute();
-$stmt = $userStmt->fetch();
+$userIdSql = "SELECT userId FROM user WHERE username = :username";
+$userStmt = $conn->prepare($userIdSql);
+$userStmt->bindParam(':username', $username, PDO::PARAM_STR);
+$userStmt->execute();
+$userId = $userStmt->fetchColumn();
 
 $itemTypeIDSql = "SELECT itemTypeId FROM itemType WHERE itemType = 'textNote'";
-$stmt = $conn->prepare($itemTypeIDSql);
-$stmt->bindParam(':itemTypeId', $itemTypeId, PDO::PARAM_STR);
-$stmt->execute();
-$itemTypeId = $stmt->fetch();
+$itemTypeStmt = $conn->prepare($itemTypeIDSql);
+$itemTypeStmt->execute();
+$itemTypeId = $itemTypeStmt->fetchColumn();
 
 $itemDetail = $_POST['textNote'];
 
-// $sql = "INSERT INTO userNotes (username, textNote) VALUES (:username, :textNote)";
-$sql = "INSERT INTO item (itemDetail,itemTypeId,userId) VALUES (:itemDetail,:itemTypeId,:userId)";
+$sql = "INSERT INTO item (itemDetail, itemTypeId, userId) VALUES (:itemDetail, :itemTypeId, :userId)";
 $stmt = $conn->prepare($sql);
 
 $stmt->bindParam(':itemDetail', $itemDetail, PDO::PARAM_STR);
-$stmt->bindParam(':itemTypeId', $itemTypeId, PDO::PARAM_STR);
-$stmt->bindParam(':userId', $userId, PDO::PARAM_STR);
+$stmt->bindParam(':itemTypeId', $itemTypeId, PDO::PARAM_INT);
+$stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
 
 if ($stmt->execute()) {
     echo '<script>alert("Text Note Saved");</script>';
     echo '<script>window.location.href = "../view/workspace.view.php";</script>';
-} 
-else 
-{
+} else {
     echo '<script>alert("Error: ' . $stmt->errorInfo()[2] . '");</script>';
 }
 ?>
