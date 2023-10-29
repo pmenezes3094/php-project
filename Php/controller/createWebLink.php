d1a32f73862cd42fc2b14b680b5d928b0a416fb9
pmenezes3094 <pmene.nic@gmail.com>
2023-10-28 21:18:29 -0700

    create database connection

--- Php/controller/createWebLink.php
+++ Php/controller/createWebLink.php
@@ -2,4 +2,12 @@
 session_start(); 
 $userId = $_SESSION['userId'];
 
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$dbusername = 'da'; 
+$dbpassword = 123; 
+
+$conn = new PDO($dsn, $dbusername, $dbpassword);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
