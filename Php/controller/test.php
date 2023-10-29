0d09bba67fea1695fb81a573fa0449a43b85ed7f
pmenezes3094 <pmene.nic@gmail.com>
2023-10-28 20:06:29 -0700

    copy workspace code in test file

---
+++ Php/controller/test.php
@@ -0,0 +1,47 @@
+<?php
+$userId = $_SESSION['userId'];
+
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$dbusername = 'da'; 
+$dbpassword = 123; 
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = "SELECT itemId, itemDetail, itemTypeId FROM item WHERE userId = :userId";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':userId', $userId, PDO::PARAM_STR);
+$stmt->execute();
+
+$result = $stmt->fetchAll();
+
+if (count($result) > 0) 
+{
+    $notes = $result;
+    foreach ($notes as $note) 
+    {
+        $itemDetail = $note['itemDetail'];
+        $itemId = $note['itemId'];
+        $itemTypeId = $note['itemTypeId'];
+            $cardHTML = "
+                <div class='card-grid-item'>
+                    <div class='card-content'>
+                        <p>
+                            $itemDetail
+                        </p>
+                    </div>
+                    <form action='../controller/deleteCard.php' method='post'>
+                        <input type='hidden' name='itemId' value='$itemId'>
+                        <button type='submit'>Delete</button>
+                    </form>
+                </div>
+            ";
+            echo $cardHTML;
+    }
+}
+else
+{
+    //else do nothing
+}
+?>
