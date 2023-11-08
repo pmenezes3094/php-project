34dc5e79a77d81404c30f070d7766175df58daea
pmenezes3094 <pmene.nic@gmail.com>
2023-11-08 00:22:46 -0800

    search retrival logic

--- Php/controller/search.php
+++ Php/controller/search.php
@@ -13,11 +13,36 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $searchItem = $_POST['searchKey'];
 
-$searchSql = "SELECT i.itemDetail,i.itemTypeId,it.itemType FROM `tag` t,item i,itemtype it where t.tagId=i.tagId and t.tagName='hii'
+$searchSql = "SELECT i.itemDetail,i.itemTypeId,it.itemType FROM `tag` t,item i,itemtype it where t.tagId=i.tagId and t.tagName='$searchItem'
 and it.itemTypeId=i.itemTypeId";
 $searchStmt = $conn->prepare($searchSql);
 $searchStmt->execute();
-$searchResult = $searchStmt->fetch();
-$searchItemDetail = $searchResult['itemDetail'];
+$searchResult = $searchStmt->fetchAll();
 
+if (count($searchResult) > 0)
+{
+    foreach($searchResult as $Result)
+    {
+        $searchItemDetail = $Result['itemDetail'];
+        $searchItemType = $Result['itemType'];
+
+        if ($searchItemType === 'textNote') 
+        {
+            $cardHTML = "
+            <div class='card-grid-item'>
+                <div class='card-content'>
+                    <p>
+                        $searchItemDetail
+                    </p>
+                </div>
+                <form action='../controller/deleteCard.php' method='post'>
+                    <input type='hidden' name='itemId' value='$searchItemType'>
+                    <button type='submit'>Delete</button>
+                </form>
+            </div>
+        ";
+        }
+        echo $cardHTML;
+    }
+}
 ?>
