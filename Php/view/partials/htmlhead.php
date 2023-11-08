614c2a1ccb53849a709664bde03ba9f1234bbe1a
pmenezes3094 <pmene.nic@gmail.com>
2023-11-07 23:24:28 -0800

    update portal title

--- Php/view/partials/htmlhead.php
+++ Php/view/partials/htmlhead.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Styles/style.css">
-    <title>Password Reset</title>
+    <title>Project Abundance</title>
     <script src="../../Scripts/cardactions.js"></script>
     <script src="../../Scripts/modals.js"></script>
     <script src="../../Scripts/utilities.js"></script>
