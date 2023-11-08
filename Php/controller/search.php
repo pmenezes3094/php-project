22b4af4ee3fc2f3fae959f33aae074687154d41c
pmenezes3094 <pmene.nic@gmail.com>
2023-11-07 23:32:46 -0800

    create database connection

---
+++ Php/controller/search.php
@@ -0,0 +1,15 @@
+<?php 
+
+session_start(); 
+$userId = $_SESSION['userId'];
+
+$dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
+$dbusername = 'da'; 
+$dbpassword = 123; 
+
+$conn = new PDO($dsn, $dbusername, $dbpassword);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?>
