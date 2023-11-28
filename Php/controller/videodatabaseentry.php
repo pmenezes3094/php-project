5ad43587643c92d4de30786fe42c34390c50d4f8
pmenezes3094 <pmene.nic@gmail.com>
2023-11-27 22:16:14 -0800

    add code for video and video tag entry

--- Php/controller/videodatabaseentry.php
+++ Php/controller/videodatabaseentry.php
@@ -1,17 +1,19 @@
 <?php
-session_start(); 
+// session_start(); 
 $userId = $_SESSION['userId'];
 $filepath = $_SESSION['filepath'];
 
-// $dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
-// $dbusername = 'da'; 
-// $dbpassword = 123; 
+$videoTags = $_POST['videoTags']; // Change variable name to match the form field name
 
 require 'database.php';
 
 $conn = new PDO($dsn, $dbusername, $dbpassword);
-
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$tagsql = "INSERT INTO tag (tagName) VALUES (:videoTags)";
+$stmt = $conn->prepare($tagsql);
+$stmt->bindParam(':videoTags', $videoTags, PDO::PARAM_STR);
+$stmt->execute();
 
 $itemTypeSql = "SELECT itemTypeId FROM itemtype WHERE itemtype = 'video'";
 $itemTypeStmt = $conn->prepare($itemTypeSql);
@@ -19,11 +21,18 @@ $itemTypeStmt->execute();
 $itemTypeResult = $itemTypeStmt->fetch();
 $itemTypeId = $itemTypeResult['itemTypeId'];
 
-$sql = "INSERT INTO item (itemDetail, itemTypeId, userId) VALUES (:itemDetail, :itemTypeId, :userId)";
+$tagIdSql = "SELECT tagId FROM tag WHERE tagName = '$videoTags'";
+$tagIdStmt = $conn->prepare($tagIdSql);
+$tagIdStmt->execute();
+$tagIdResult = $tagIdStmt->fetch();
+$tagId = $tagIdResult['tagId'];
+
+$sql = "INSERT INTO item (itemDetail, itemTypeId, userId, tagId) VALUES (:itemDetail, :itemTypeId, :userId, :tagId)";
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':itemDetail', $filepath, PDO::PARAM_STR);
 $stmt->bindParam(':itemTypeId', $itemTypeId, PDO::PARAM_INT);
 $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
+$stmt->bindParam(':tagId', $tagId, PDO::PARAM_INT);
 
 $stmt->execute();
 ?>
