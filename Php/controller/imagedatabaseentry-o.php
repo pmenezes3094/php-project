aedcfc79bf4ff354febdea71c1c196ff7ba80ae0
pmenezes3094 <pmene.nic@gmail.com>
2023-11-27 21:21:23 -0800

    add code for tagging images

--- Php/controller/imagedatabaseentry-o.php
+++ Php/controller/imagedatabaseentry-o.php
@@ -1,9 +1,7 @@
 <?php
-// session_start(); 
+session_start(); 
 $userId = $_SESSION['userId'];
 $filepath = $_SESSION['filepath'];
-
-$imageTags = $_POST['imageTags'];
 
 // $dsn = "mysql:host=localhost;port=3306;dbname=phpproject;charset=utf8mb4";
 // $dbusername = 'da'; 
@@ -15,29 +13,17 @@ $conn = new PDO($dsn, $dbusername, $dbpassword);
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$tagsql ="INSERT INTO tag (tagName) VALUES (:imageTags)";
-$stmt = $conn->prepare($tagsql);
-$stmt->bindParam(':imageTags', $imageTags, PDO::PARAM_STR);
-$stmt->execute();
-
 $itemTypeSql = "SELECT itemTypeId FROM itemtype WHERE itemtype = 'image'";
 $itemTypeStmt = $conn->prepare($itemTypeSql);
 $itemTypeStmt->execute();
 $itemTypeResult = $itemTypeStmt->fetch();
 $itemTypeId = $itemTypeResult['itemTypeId'];
 
-$tagIdSql = "SELECT tagId FROM tag WHERE tagName = '$imageTags'";
-$tagIdStmt = $conn->prepare($tagIdSql);
-$tagIdStmt->execute();
-$tagIdResult = $tagIdStmt->fetch();
-$tagId = $tagIdResult['tagId'];
-
-$sql = "INSERT INTO item (itemDetail, itemTypeId, userId, tagId) VALUES (:itemDetail, :itemTypeId, :userId, :tagId)";
+$sql = "INSERT INTO item (itemDetail, itemTypeId, userId) VALUES (:itemDetail, :itemTypeId, :userId)";
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':itemDetail', $filepath, PDO::PARAM_STR);
 $stmt->bindParam(':itemTypeId', $itemTypeId, PDO::PARAM_INT);
 $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
-$stmt->bindParam(':tagId', $tagId, PDO::PARAM_INT);
 
 $stmt->execute();
 ?>
